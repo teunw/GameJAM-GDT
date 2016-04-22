0cf3c112d658a6a6ef219c0d8225e9da11ee20d4
Teun <teun.willems@live.nl>
2016-04-22 15:02:55 +0200

    Added libs

--- GameJamDrawSomething/Assets/Scripts/DrawManager.cs
+++ GameJamDrawSomething/Assets/Scripts/DrawManager.cs
@@ -1,5 +1,7 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System;
+using Assets.Scripts;
+using UnityEngine;
+using Quobject.SocketIoClientDotNet.Client;
 
 public class DrawManager : MonoBehaviour
 {
@@ -14,10 +16,33 @@ public class DrawManager : MonoBehaviour
 
     public bool doDraw;
 
+    private Socket socket;
+
     // Use this for initialization
     void Start()
     {
+        socket = IO.Socket("ws://teunwillems.nl:3000/");
 
+        socket.On(Socket.EVENT_ERROR, () =>
+        {
+            Debug.Log("Error");
+        });
+        socket.On(Socket.EVENT_CONNECT, () =>
+        {
+            Debug.Log("Connected");
+            socket.Emit("UnityClient", "asdf");
+        });
+        socket.On("location", (data) =>
+        {
+            Debug.Log(data);
+            Location loc = JsonUtility.FromJson<Location>(data.ToString());
+            Debug.Log(data.ToString());
+        });
+    }
+
+    void OnDestroy()
+    {
+        socket.Close();
     }
 
     // Update is called once per frame
@@ -62,6 +87,19 @@ public class DrawManager : MonoBehaviour
         mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));
         line.SetPosition(0, mousePos);
         line.SetPosition(1, mousePos);
+        line.transform.SetParent(dotParent.transform);
+    }
+
+    public void createLine(Vector3 loc1, Vector3 loc2)
+    {
+        line = new GameObject("Line " + currLines).AddComponent<LineRenderer>();
+        line.material = material;
+        line.SetVertexCount(2);
+        line.SetWidth(0.08f, 0.08f);
+        line.useWorldSpace = true;
+        mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));
+        line.SetPosition(0, loc1);
+        line.SetPosition(1, loc2);
         line.transform.SetParent(dotParent.transform);
     }
 
