8cc355845e8e68b28f77f79666adbe4069084814
Teun <teun.willems@live.nl>
2016-04-22 15:10:33 +0200

    Fixed commit errors

--- GameJamDrawSomething/Assets/Scripts/DrawManager.cs
+++ GameJamDrawSomething/Assets/Scripts/DrawManager.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using Assets.Scripts;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
 using Quobject.SocketIoClientDotNet.Client;
 
 public class DrawManager : MonoBehaviour
 {
-    public GameObject dotParent;
+    public static DrawManager instance;
 
+    public Canvas Canvas;
+    public GameObject dotParent;
+    public RectTransform panel;
     //Copy Pasta'd Code
     private LineRenderer line;
     private Vector3 mousePos;
@@ -16,7 +19,8 @@ public class DrawManager : MonoBehaviour
 
     public bool doDraw;
 
-    private Socket socket;
+    // Use this for initialization
+        private Socket socket;
 
     // Use this for initialization
     void Start()
@@ -53,10 +57,30 @@ public class DrawManager : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (!Input.GetMouseButton(0) || !doDraw) // or screen touched
+        if (!Input.GetMouseButton(0) || !doDraw)
         {
             line = null;
             return;
+        }
+        if (doDraw)
+        {
+            mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));
+            Vector2 mpv2 = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            Vector2 min = Vector2.zero;
+            Vector2 max = panel.rect.max * 2;
+            
+            //Right & Top
+            max = new Vector2(max.x + panel.offsetMin.x, max.y + panel.offsetMin.y);
+            //Left & Bottom
+            min = new Vector2(min.x + panel.offsetMin.x, min.y + panel.offsetMin.y);
+            max *= Canvas.scaleFactor;
+            min *= Canvas.scaleFactor;
+            //min = new Vector2(min.x + panel.rect.left, min.y + panel.rect.top);
+            if (!(min.x <= mpv2.x && max.x > mpv2.x && min.y <= mpv2.y && max.y > mpv2.y))
+            {
+                line = null;
+                return;
+            }
         }
 
         //Create line
@@ -67,7 +91,7 @@ public class DrawManager : MonoBehaviour
         }
         else
         {
-            mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));
+            
             line.SetPosition(1, mousePos);
             line = null;
             currLines++;
@@ -84,27 +108,28 @@ public class DrawManager : MonoBehaviour
         line.SetVertexCount(2);
         line.SetWidth(0.08f, 0.08f);
         line.useWorldSpace = true;
-        mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));
         line.SetPosition(0, mousePos);
         line.SetPosition(1, mousePos);
         line.transform.SetParent(dotParent.transform);
-    }
-
-    public void createLine(Vector3 loc1, Vector3 loc2)
-    {
-        line = new GameObject("Line " + currLines).AddComponent<LineRenderer>();
-        line.material = material;
-        line.SetVertexCount(2);
-        line.SetWidth(0.08f, 0.08f);
-        line.useWorldSpace = true;
-        mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));
-        line.SetPosition(0, loc1);
-        line.SetPosition(1, loc2);
-        line.transform.SetParent(dotParent.transform);
+        line.transform.position += new Vector3(0, 0, -0.01f);
     }
 
     public void setDraw()
     {
         doDraw = !doDraw;
+    }
+
+    public void clearDrawings()
+    {
+        foreach (Transform l in dotParent.transform)
+        {
+            Destroy(l.gameObject);
+        }
+        currLines = 0;
+    }
+
+    public void setColor(Material color)
+    {
+        material = color;
     }
 }
